d89009920c2797bc0a03113873970d9fe80ac75f
zhaoqp2010 <zhaoqp2010@163.com>
2015-06-28 00:20:33 +0800

    dialog在个别时候关闭失败的bug,增加http请求的缓存方法

---
+++ AndBaseDemo/src/com/andbase/model/AppUser.java
@@ -0,0 +1,96 @@
+package com.andbase.model;
+
+public class AppUser {
+
+	private int appId;
+	private String imei;
+	private String ismi;
+	private String province;
+	private String city;
+	private String address;
+	private String longitude;
+	private String latitude;
+	private String qq;
+	private String appTime;
+
+	public int getAppId() {
+		return appId;
+	}
+
+	public void setAppId(int appId) {
+		this.appId = appId;
+	}
+
+	public String getImei() {
+		return imei;
+	}
+
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+
+	public String getIsmi() {
+		return ismi;
+	}
+
+	public void setIsmi(String ismi) {
+		this.ismi = ismi;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getQq() {
+		return qq;
+	}
+
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+
+	public String getAppTime() {
+		return appTime;
+	}
+
+	public void setAppTime(String appTime) {
+		this.appTime = appTime;
+	}
+
+}
